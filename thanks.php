ad621981fd5125e9eb0b558b826f13945c646625
Morgane Jumel <53408984+MorganeJumel@users.noreply.github.com>
2020-10-05 21:51:37 +0200

    Update thanks.php

--- thanks.php
+++ thanks.php
@@ -1,4 +1,5 @@
 <?php
    
-  echo 'Thank you ' . $_GET['firstName'] .' '. $_GET['lastName'] . ' for getting in touch with us about: ' .$_GET['topic'] . '. <br> One of our team member will contact you shortly, either via your email address: ' . $_GET['userMail'] . ', or via the phone number you\'ve provided: ' .$_GET['userPhone'] .'
-   <br> Reminder pour message:'.$_GET['userMessage']; 
+   echo 'Thank you ' . $_GET['firstName'] .' '. $_GET['lastName'] . ' for getting in touch with us about: ' .$_GET['topic'] . '. <br> One of our team member will contact you shortly, either via your email address: ' . $_GET['userMail'] . ', or via the phone number you\'ve provided: ' .$_GET['userPhone'] .'
+   <br> Your message:'.$_GET['userMessage']; 
+
